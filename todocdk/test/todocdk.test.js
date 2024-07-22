ce6d45f68c795f6e23d429f9cc739042db295004
PetarNachev <p.nachev92@gmail.com>
2024-07-22 11:42:41 +0300

    Initializing CDK
    Setting up a CDK project for the backend of the Todo App

---
+++ todocdk/test/todocdk.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const Todocdk = require('../lib/todocdk-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/todocdk-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new Todocdk.TodocdkStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
