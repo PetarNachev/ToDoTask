f71dab8995e408c01097ddb05908dfe4688a4d85
PetarNachev <p.nachev92@gmail.com>
2024-07-22 15:27:26 +0300

    Fetch todo req
    Adding the fetch request to the backend api

--- todoreact/src/api/todoService.js
+++ todoreact/src/api/todoService.js
@@ -1,3 +1,15 @@
 import axios from 'axios';
 
 const API_ENDPOINT = 'https://t337uvx41k.execute-api.eu-north-1.amazonaws.com/prod/';
+
+
+export const fetchTodos = async () => {
+    try {
+        const response = await axios.get(`${API_ENDPOINT}/todos`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching todos:', error);
+        throw error;
+    }
+};
+
