588f137f40916cd527a6a81d6c0f37181c172bd8
PetarNachev <p.nachev92@gmail.com>
2024-07-22 13:05:22 +0300

    Adding CreateToDo Lambda
    Setting up the Create functionality, added dynamod util lib, crypto for the ID and PutItemCommand

--- todocdk/src/lambdas/todolambda.js
+++ todocdk/src/lambdas/todolambda.js
@@ -1,4 +1,52 @@
-const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
+const crypto = require('crypto');
 
 
 const client = new DynamoDBClient();
+
+function generateId() {
+    return crypto.randomBytes(16).toString('hex');
+}
+
+const currentTimestamp = new Date().toISOString();
+
+
+exports.createTodo = async (event) => {
+
+    const id = generateId();
+
+    let body = JSON.parse(event.body)
+    const params = {
+        TableName: 'ToDoTable',
+        Item: marshall({
+            id,
+            title: body.title,
+            description: body.description,
+            lastUpdated: currentTimestamp,
+        }),
+    };
+
+    const command = new PutItemCommand(params);
+
+    try {
+        const result = await client.send(command);
+        console.log('DynamoDB result:', JSON.stringify(result))
+
+        const item = result.Item ? unmarshall(result.Item) : null;
+
+        return {
+            statusCode: 201,
+            body: JSON.stringify({ message: 'Todo created successfully', item }),
+        };
+
+    } catch (error) {
+        console.error(error)
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Error creating todo" })
+        }
+
+    }
+};
